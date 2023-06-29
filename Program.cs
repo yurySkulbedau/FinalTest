6803abc5baca09733c8e6b06b9979c359a58d9cc
Jerry <yury@skulbedau.by>
2023-06-29 14:19:08 +0300

    set the initial arrays

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.
+
+Примеры:
+["Hello", "2", "world", ":-)"] → ["2", ":-)"]
+["1234", "1567", "-2", "computer science"] → ["-2"]
+["Russia", "Denmark", "Kazan"] → []
+*/
+
+string[] line1 = new string ["Hello", "2", "world", ":-)"];
+string[] line2 = new string ["1234", "1567", "-2", "computer science"];
+string[] line3 = new string ["Russia", "Denmark", "Kazan"];
+
